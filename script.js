8490543d8879effeee971e9189960b7add5d7501
Ryan Whitcomb <ryanww321@gmail.com>
2020-01-24 08:50:12 -0600

    additional fixes to the js file

--- script.js
+++ script.js
@@ -1,55 +1,106 @@
-// Generator functions http://www.net-comber.com/charset.html
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*()+=?><.';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Special characters for the function created
+const specialCharacters = '!@#$%^&*()+=?><.';
 
 // Write password to the #password input
 function writePassword() {
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  };
-
   passwordText.value = password;
-  
-  
-// Function for generating the random password
 
-function generatePassword() {
-
-var passwordLength = prompt("Between 8 & 128 characters, how long do you want your password to be?");
-
-var numbers = confirm("Do you want numbers in your password?");
-
-var lowerCases = confirm("Do you want lowercases in your password?");
-
-var upperCases = confirm("Do you want uppercases in your password?");
-
-var specialCharacters = confirm("Do you want special characters in your password?");
-
-
-   
- }
-
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompts that come up after you click generate password
+function generatePassword() {
+  var passwordLength = prompt("Are you okay with a password between 8 & 128 characters?");
+
+  var numbers = confirm("Do you want numbers in your password?");
+
+  var lowerCases = confirm("Do you want lowercases in your password?");
+
+  var upperCases = confirm("Do you want uppercases in your password?");
+
+  var specialCharacters = confirm("Do you want special characters in your password?");
+
+  // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 0;
+
+
+  // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
+
+  var minimumNumbers = "";
+  var minimumLowerCases = "";
+  var minimumUpperCases = "";
+  var minimumSpecialCharacters = "";
+
+
+
+  // Generator functions
+  var functionArray = [
+    function getNumbers() {
+      return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    },
+    
+    function getLowerCases() {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    },
+
+    function getUpperCases() {
+      return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    },
+
+    function getSpecialCharacters() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    }
+
+  ]
+
+  // Checks to make sure user selected ok for all and uses empty minimums from above
+
+  if (numbers === true) {
+    minimumNumbers = functionArray[0]; minimumCount++;
+
+  }
+
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray[1]; minimumCount++;
+
+  }
+
+  if (upperCases === true) {
+    minimumUpperCases = functionArray[2]; minimumCount++;
+
+  }
+
+  if (specialCharacters === true) {
+    minimumSpecialCharacters = functionArray[3]; minimumCount++;
+
+  }
+
+  // empty string variable for the for loop below
+  var randomPasswordGenerated = "";
+
+  // loop getting random characters
+  for (let i = 0; i < (parseInt(passwordLength) - minimumCount); i++) {
+    var randomNumberPicked = Math.floor(Math.random() * 4)
+
+    randomPasswordGenerated += functionArray[randomNumberPicked]();
+
+  }
+
+  // to make sure characters are added to the password
+  randomPasswordGenerated += minimumNumbers;
+  randomPasswordGenerated += minimumLowerCases;
+  randomPasswordGenerated += minimumUpperCases;
+  randomPasswordGenerated += minimumSpecialCharacters;
+
+
+  return randomPasswordGenerated;
+
+}
